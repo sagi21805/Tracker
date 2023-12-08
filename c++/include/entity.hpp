3629d4b3d7d76976f913c90ca1628753ea627e82
sagi <sagi21805@gmail.com>
2023-12-08 22:51:16 +0200

    Added Documentation and organized

--- c++/include/entity.hpp
+++ c++/include/entity.hpp
@@ -5,61 +5,253 @@
 #include <limits>
 #include "vectorFuncs.hpp"
 
+//////////////////////////////////////////// Entity //////////////////////////////////////////////////////
+
+/**
+ * @file entity.hpp
+ * @brief The Entity class.
+ * @paragraph 
+ *  The Entity class contains an easy way to create an Entity from YOLO_DATA 
+ *  The contains easy way to gather information like trajectory, velocity, and position of the Entity, 
+ *  as well as prediction methods based on that info.
+ *  
+ * @author Sagi Or
+*/  
 class Entity{
 
     private:
 
-        uint16_t id;
-        uint16_t type;
-        Velocity2D velocities;
-        Rect boundingRect;
-        std::shared_ptr<LinkedList> trajectory;
+        uint16_t id; //The id of the Entity **cannot be changed outside of Entity after creation**
+        uint16_t type; //The Type/Class of the Entity **cannot be changed outside of Entity after creation**
+        Velocity2D velocity; //The Velocity (x, y) of the Entity in pixels per frame
+        Rect boundingRect; //The bounding rectangle of the Entity in pixels
+        std::shared_ptr<LinkedList> trajectory; //the Trajectory of the Entity which contains previous boundingRect and velocity
 
     public:
         
-        Entity(uint16_t id, uint16_t type, Rect BoundingRect);
+///////////////////////////////////////// Constructors //////////////////////////////////////////////////
 
+        /**
+         * @file entity.cpp
+         * @brief Entity constractor from raw data.
+         * @param id The id of the Entity.
+         * @param type The type of the Entity.
+         * @param BoundingRect The boundingRect of the Entity.
+         * @author Sagi Or
+        */
+        Entity(uint16_t id, uint16_t type, Rect boundingRect);
+
+        /**
+         * @file entity.cpp
+         * @brief Entity constractor from another Entity.
+         * @param e The Entity to construct from.
+         * @author Sagi Or
+        */
         Entity(const Entity& e);
 
+        /**
+         * @file entity.cpp
+         * @brief Entity defualt constractor.
+         * @author Sagi Or
+        */
         Entity();
+
         
-        void setBoundingRect(Rect newBoundingRect);
+///////////////////////////////////////// Get Functions //////////////////////////////////////////////////
 
+        /**
+         * @file entity.cpp
+         * @brief Get Entity's BoundingRect
+         * @returns Reference to the Entity's BoundingRect.
+         * @author Sagi Or
+        */
         Rect& getBoundingRect();
-
+        
+        /**
+         * @file entity.cpp
+         * @brief Get Entity's id.
+         * @returns The Entity's id.
+         * @author Sagi Or
+        */
         uint16_t getId();
 
+        /**
+         * @file entity.cpp
+         * @brief Get Entity's type.
+         * @returns The Entity's type.
+         * @author Sagi Or
+        */
         uint16_t getType();
+        
+        /**
+         * @file entity.cpp
+         * @brief Get Entity's Velocity.
+         * @returns Reference To the Entity's velocity.
+         * @author Sagi Or
+        */
+        Velocity2D& getVelocity();
 
-        Velocity2D getVelocities();
+        /**
+         * @file entity.cpp
+         * @brief Get Entity's weak_ptr to the Entity's Trajectory.
+         * @returns Weak_ptr the Entity's Trajectory.
+         * @author Sagi Or
+        */
+        std::weak_ptr<LinkedList> getTrajectory();
+        
+        /**
+         * @file entity.cpp
+         * @brief Get Entity's a shared_ptr to the Entity's Trajectory.
+         * @returns Shared_ptr to the Entity's Trajectory.
+         * @author Sagi Or
+        */
+        std::shared_ptr<LinkedList> copyTrajectory();
 
-        std::shared_ptr<LinkedList> getTrajetctory();
-
+        /**
+         * @file entity.cpp
+         * @brief Get Entity's BoundingRect.
+         * @returns copy of the Entity's BoundingRect.
+         * @author Sagi Or
+        */
         Rect getBoundingRect() const;
 
+        /**
+         * @file entity.cpp
+         * @brief Get the Entity's id.
+         * @returns The Entity's id.
+         * @author Sagi Or
+        */
         uint16_t getId() const;
 
+        /**
+         * @file entity.cpp
+         * @brief Get the Entity's type.
+         * @returns The Entity's type.
+         * @author Sagi Or
+        */
         uint16_t getType() const;
 
-        std::shared_ptr<LinkedList> getTrajetctory() const;
+        /**
+         * @file entity.cpp
+         * @brief Get the Entity's Velocity.
+         * @returns The Entity's velocity.
+         * @author Sagi Or
+        */
+        Velocity2D getVelocity() const;
 
+        /**
+         * @file entity.cpp
+         * @brief Get Entity's a weak_ptr to the Entity's Trajectory.
+         * @returns Weak_ptr the Entity's Trajectory.
+         * @author Sagi Or
+        */
+        std::weak_ptr<LinkedList> getTrajectory() const;
+
+        /**
+         * @file entity.cpp
+         * @brief Get Entity's a shared_ptr to the Entity's Trajectory.
+         * @returns Shared_ptr the Entity's Trajectory.
+         * @author Sagi Or
+        */
+        std::shared_ptr<LinkedList> copyTrajectory() const;
+
+
+///////////////////////////////////////// Set Functions //////////////////////////////////////////////////
+
+        /**
+         * @file entity.cpp
+         * @brief Sets the boundingRect of the Entity to a given Rect.
+         * @param newBoundingRect The new BoundingRect to set.
+         * @author Sagi Or
+        */
+        void setBoundingRect(Rect newBoundingRect);
+
+        /**
+         * @file entity.cpp
+         * @brief Sets the Velocity of the Entity to a given Velocity.
+         * @param velocity The new velocity to set.
+         * @author Sagi Or
+        */
+        void setVelocity(Velocity2D velocity);
+
+
+///////////////////////////////////////// Util Functions //////////////////////////////////////////////////
+        
+        /**
+         * @file entity.cpp
+         * @brief Calculates the average Velocity on a given number of frames and returns it.
+         * @param numOfFrames The number of frames to avrage velocity on.
+         * @returns The calculated Velocity.
+         * @author Sagi Or
+        */
+        Velocity2D calcVelocity(uint8_t numOfFrames);
+
+        /**
+         * @file entity.cpp
+         * @brief Addes the current BoundingRect and Velocity to the Trajectory.
+         * @author Sagi Or
+        */
         void addToTrajectory();     
 
-        uint squareDistanceTo(Entity &e);
-        
-        uint16_t findClosestEntityIndex(std::vector<Entity> &entityVector);
-
+        /**
+         * @file entity.cpp
+         * @brief Sets the Entity's BoundingRect to EmptyRect.
+         * @author Sagi Or
+        */
         void emptyBoundingRect();
 
-        void clacVelocities(int numOfFrames);
-
-        Rect predictNextBoundingRect();
-
-        Rect possibleLocations();
+        /**
+         * @file entity.cpp
+         * @brief Calculates the Square distance between two Entities BoundingRect.
+         * @returns The Squared distance between the Entities.
+         * @author Sagi Or
+        */
+        uint squareDistanceTo(Entity &e);
         
+        /**
+         * @file entity.cpp
+         * @brief Predicts the next BoundingRect based on the past frames velocity.
+         * @param numOfFrames The number of past frames to avrage velocity on.
+         * @returns The predicted Rect.
+         * @author Sagi Or
+        */
+        Rect predictNextBoundingRect(uint8_t numOfFrame);
+
+        /**
+         * @file entity.cpp
+         * @brief Returns the index in an entityVector of the matching Entity based on predictions, distance and more! 
+         * @param numOfFrames The number of past frames data to predict on.
+         * @returns Rect of the Predicted possible locations of the TopLeft Corner of the Entity.
+         * @author Sagi Or
+        */
+        Rect predictPossibleLocations(uint8_t numOfFrames);
+        
+        /**
+         * @file entity.cpp
+         * @brief match Entity on an entity vector based on predictions, distance and more! 
+         * @param entityVector The vector to match Entities from.
+         * @param numOfFrames The number of frames to avrage velocity on.
+         * @returns The index of the matching Entity on the vector.
+         * @author Sagi Or
+        */
+        uint16_t matchEntity(std::vector<Entity> &entityVector, uint8_t numOfFrames);
+        
+        /**
+         * @file entity.cpp
+         * @brief draws the BoundingRect of the Entity on a given frame 
+         * @param frame The frame to draw the BoundingRect on.
+         * @param color The color of the BoundingRect.
+         * @author Sagi Or
+        */
         void draw(cv::Mat& frame, cv::Scalar color);
 };
 
+/**
+ * @file entity.cpp
+ * @brief Defines the operator << to print an Entity
+ * @param e The Entity to print.
+ * @author Sagi Or
+*/
 std::ostream& operator<<(std::ostream& os, const Entity& e);
 
 #endif
--- c++/src/entity.cpp
+++ c++/src/entity.cpp
@@ -1,5 +1,7 @@
 #include "entity.hpp"
 
+////////////////////////////////// constructors //////////////////////////////////////////////////
+
 Entity::Entity(uint16_t id, uint16_t type, Rect boundingRect) 
     : id(id), type(type){
     this->setBoundingRect(boundingRect);
@@ -9,7 +11,7 @@ Entity::Entity(uint16_t id, uint16_t type, Rect boundingRect)
 Entity::Entity(const Entity& e) 
     : id(e.getId()), type(e.getType()){
     this->setBoundingRect(e.getBoundingRect());
-    this->trajectory = e.getTrajetctory();
+    this->trajectory = e.copyTrajectory();
 }
 
 Entity::Entity() 
@@ -18,18 +20,7 @@ Entity::Entity()
     this->trajectory = std::make_shared<LinkedList>();
 }
 
-std::shared_ptr<LinkedList> Entity::getTrajetctory(){
-    return this->trajectory;
-}
-
-void Entity::setBoundingRect(Rect boundningRect){
-    this->boundingRect = boundningRect;
-}
-
-void Entity::addToTrajectory(){
-    std::shared_ptr<Node> newNode = std::make_shared<Node>(this->boundingRect, this->velocities);
-    this->trajectory->prepend(newNode);
-}
+////////////////////////////////// Get Functions //////////////////////////////////////////////////
 
 Rect& Entity::getBoundingRect(){
     return this->boundingRect;
@@ -43,11 +34,16 @@ uint16_t Entity::getType(){
     return this->type;
 }
 
-Velocity2D Entity::getVelocities(){
-    return this->velocities;
+Velocity2D& Entity::getVelocity(){
+    return this->velocity;
 }
 
-std::shared_ptr<LinkedList> Entity::getTrajetctory() const{
+std::weak_ptr<LinkedList> Entity::getTrajectory(){
+    std::weak_ptr<LinkedList> weak_trajectory = this->trajectory;
+    return weak_trajectory;
+}
+
+std::shared_ptr<LinkedList> Entity::copyTrajectory(){
     return this->trajectory;
 }
 
@@ -63,23 +59,64 @@ uint16_t Entity::getType() const{
     return this->type;
 }
 
-uint Entity::squareDistanceTo(Entity& e){
-    return this->getBoundingRect().squareDistanceTo(e.getBoundingRect());
+Velocity2D Entity::getVelocity() const{
+    return this->velocity;
 }
 
-std::ostream& operator<<(std::ostream& os, Entity& e){
-    os << "id: " << e.getId() << "\n";
-    os << "type: " << e.getType() << "\n";
-    os << "box: " << e.getBoundingRect() << "\n";
-    return os;
+std::weak_ptr<LinkedList> Entity::getTrajectory() const{
+    std::weak_ptr<LinkedList> weak_trajectory = this->trajectory;
+    return weak_trajectory;
+}
+
+std::shared_ptr<LinkedList> Entity::copyTrajectory() const{
+    return this->trajectory;
+}
+
+void Entity::setBoundingRect(Rect boundningRect){
+    this->boundingRect = boundningRect;
+}
+
+void Entity::setVelocity(Velocity2D velocity){
+    this->velocity = velocity;
+}
+
+Velocity2D Entity::calcVelocity(uint8_t numOfFrames){
+
+    if (this->trajectory->length >= numOfFrames){
+        const Rect& startRect = this->trajectory->getItem(numOfFrames - 1).rect;
+        const Rect& endRect = this->trajectory->getItem(0).rect;
+        cout << "endX: " << endRect.x << " startX: " << startRect.x << "\n";
+        float velX = (float) (endRect.x - startRect.x) / numOfFrames;
+        float velY = (float) (endRect.y - startRect.y) / numOfFrames;
+        return Velocity2D(velX, velY);
+    } 
+
+    return Velocity2D();
+}
+
+void Entity::addToTrajectory(){
+    std::shared_ptr<Node> newNode = std::make_shared<Node>(this->boundingRect, this->velocity);
+    this->trajectory->prepend(newNode);
 }
 
 void Entity::emptyBoundingRect(){
     this->setBoundingRect(Rect());
 }
 
-Rect Entity::possibleLocations(){
-    this->clacVelocities(3);
+uint Entity::squareDistanceTo(Entity& e){
+    return this->getBoundingRect().squareDistanceTo(e.getBoundingRect());
+}
+
+Rect Entity::predictNextBoundingRect(uint8_t numOfFrames){
+    this->calcVelocity(numOfFrames);
+    Rect prediction = this->boundingRect;
+    prediction.x += this->velocity.x;
+    prediction.y += this->velocity.y;
+    return prediction;
+}
+
+Rect Entity::predictPossibleLocations(uint8_t numOfFrames){
+    this->calcVelocity(numOfFrames);
     Rect& currentRect = this->getBoundingRect();
     const int& W = currentRect.width;
     const int& H = currentRect.height;
@@ -89,31 +126,28 @@ Rect Entity::possibleLocations(){
     // const float M = 5; //TODO MAKE SOMEHOW CALCULATED
     Rect possibleLocations = Rect(currentRect.tl() - Point(K * W,  K * H), cv::Size2i(J * W, J * H));
 
-    // if (this->velocities.x != 0 || this->velocities.y != 0){
+    // if (this->velocity.x != 0 || this->velocity.y != 0){
     //     cout << this->id << "\n";
-    //     uint16_t x = currentRect.tl().x + N * this->velocities.x;
-    //     uint16_t y = currentRect.tl().y + N * this->velocities.y;
+    //     uint16_t x = currentRect.tl().x + N * this->velocity.x;
+    //     uint16_t y = currentRect.tl().y + N * this->velocity.y;
     //     Point A = Point(x, y);
-    //     x = currentRect.tl().x - M * this->velocities.x;
-    //     y = currentRect.tl().y - M * this->velocities.y;
+    //     x = currentRect.tl().x - M * this->velocity.x;
+    //     y = currentRect.tl().y - M * this->velocity.y;
     //     Point B = Point(x, y);
-    //     cout << "tl: " << currentRect.tl() << " A: " << A << " B: " << B << " vel: " << this->velocities << "\n";
+    //     cout << "tl: " << currentRect.tl() << " A: " << A << " B: " << B << " vel: " << this->velocity << "\n";
     //     possibleLocations = Rect(A, B);
     // }
-
-
-
     return possibleLocations;
 }
 
-uint16_t Entity::findClosestEntityIndex(std::vector<Entity>& entityVector){
+uint16_t Entity::matchEntity(std::vector<Entity>& entityVector, uint8_t numOfFrames){
 
     // uint maxDistanceSquared = 500; //TODO MAKE A SOMEHOW CALCULATED ONE
     uint distanceSquared = UINT32_MAX;
     uint16_t idx = entityVector.size(); 
 
-    Rect possibleLocations = this->possibleLocations();
-
+    Rect possibleLocations = this->predictPossibleLocations(numOfFrames);
+    
     for (uint16_t i = 0, size = entityVector.size(); i < size; i++){
         Entity& checkedEntity = entityVector[i];
 
@@ -128,32 +162,10 @@ uint16_t Entity::findClosestEntityIndex(std::vector<Entity>& entityVector){
     return idx;
 }
 
-void Entity::clacVelocities(int numOfFrames){
-    //units are pixels per frame   
-    if (this->trajectory->length >= numOfFrames){
-        const Rect& startRect = this->trajectory->getItem(numOfFrames - 1).rect;
-        const Rect& endRect = this->trajectory->getItem(0).rect;
-        cout << "endX: " << endRect.x << " startX: " << startRect.x << "\n";
-        int velX = (endRect.x - startRect.x) / numOfFrames;
-        int velY = (endRect.y - startRect.y) / numOfFrames;
-        this->velocities = Velocity2D(velX, velY);
-    } 
-    
-}
-
-
-Rect Entity::predictNextBoundingRect(){
-    Rect prediction = this->boundingRect;
-    prediction.x += this->velocities.x;
-    prediction.y += this->velocities.y;
-    return prediction;
-}
-
 void Entity::draw(cv::Mat& frame, cv::Scalar color){
 		this->getBoundingRect().drawRect(frame, color);
 		cv::putText(frame, std::to_string(this->id), this->boundingRect.tl(), cv::FONT_HERSHEY_DUPLEX, 1, CV_RGB(255, 255, 0), 2);
 }
-
 
 std::ostream& operator<<(std::ostream& os, const Entity& t){
     os << "id: " << t.getId() << "\n";
