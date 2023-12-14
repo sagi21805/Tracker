8b566325ab4a63efb813624fe2bb1bb7da77d231
sagi <sagi21805@gmail.com>
2023-12-14 12:55:42 +0200

    Added new Parameters

--- c++/include/config.hpp
+++ c++/include/config.hpp
@@ -4,12 +4,39 @@
 #include <string>
 #include <fstream>
 #include <nlohmann/json.hpp>
+#include "vectorFuncs.hpp"
+#include <opencv4/opencv2/opencv.hpp>
+
+
+using json = nlohmann::json;
 
 // Declare global variables
 extern uint8_t _pointSize;  
 extern bool _visualize;
 extern uint16_t _waitKey;
+extern uint8_t _numOfFrames;
+extern float _predictionOffsetCoefficient;
+extern float _predictionSizeCoefficient;
+extern vector<cv::Scalar> _colors;
+extern float _convolutionThresh;
 // Function to initialize global variables from JSON
 void config(const std::string& filename);
+
+//the nested data must be the same type. 
+template<typename T> vector<T> readNestedData(json nestedData){
+
+    vector<T> outVector;
+    cout << "size: " << nestedData.size() << "\n";
+    outVector.reserve(nestedData.size());
+
+    // add type checking on the value.
+    for (auto it = nestedData.begin(); it != nestedData.end(); ++it) {
+        const json& value = it.value();
+        cv::Scalar color = CV_RGB(value[0], value[1], value[2]);
+        outVector.push_back(color);
+    }
+
+    return outVector;
+}
 
 #endif
--- c++/src/config.cpp
+++ c++/src/config.cpp
@@ -1,14 +1,16 @@
 #include "config.hpp"
-
-using json = nlohmann::json;
-
 // Define global variables
+//TODO add type cheking on all of the config data. important!!!!
 
 uint8_t _pointSize;  
 bool _visualize;
 uint16_t _waitKey;
-
-// Implementation of the initialization function
+uint8_t _numOfFrames;
+float _predictionOffsetCoefficient;
+float _predictionSizeCoefficient;
+vector<cv::Scalar> _colors;
+float _convolutionThresh;
+//the initialization function
 void config(const std::string& filename){
     // Open the JSON file
     std::ifstream input_file(filename);
@@ -19,14 +21,23 @@ void config(const std::string& filename){
         return;
     }
 
-    // Parse the JSON content
     json config;
+    // Parse the JSON content
     input_file >> config;
 
-    // Check if the key "globalString" exists and has a non-null value
+     //REGULAR DATA
+
     _pointSize = config["pointSize"];
     _visualize = config["visualize"];
     _waitKey = config["waitKey"];
+    _predictionOffsetCoefficient = config["predictionOffsetCoefficient"];
+    _predictionSizeCoefficient = config["predictionSizeCoefficient"];
+    _numOfFrames = config["numOfFrames"];
+    _convolutionThresh = config["convolutionThresh"];
+    //NESTED DATA;
+
+    _colors = readNestedData<cv::Scalar>(config["colors"]);
 
     input_file.close();
 }
+
