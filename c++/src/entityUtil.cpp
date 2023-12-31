ac403a31ef66f17e4300f4741eb7bb10ac78279b
sagi <sagi21805@gmail.com>
2023-12-31 20:50:59 +0200

    Changed the calcAndSetVelocity method, to have deadband, added squared distance between a rect and an Entity, created generate Entity method, improved match Entity function

--- c++/src/entityUtil.cpp
+++ c++/src/entityUtil.cpp
@@ -2,9 +2,14 @@
 
 
 void Entity::calcAndSetVelocity(){
-    const Rect& startRect = this->trajectory->getItem(1).rect; //the place before that
-    const Rect& endRect = this->trajectory->getItem(0).rect; //current place
-    this->velocity =  Velocity2D(endRect.x - startRect.x, endRect.y - startRect.y);
+    if (this->trajectory->length > 3){
+        uint8_t numOfFrames = 3;
+        const Rect& startRect = this->trajectory->getItem(numOfFrames).rect; //the place before 
+        const Rect& endRect = this->trajectory->getItem(0).rect; //current place
+        Velocity2D rawVelocity((endRect.x - startRect.x) / numOfFrames, (endRect.y - startRect.y) / numOfFrames);
+        this->velocity.x = applyDeadband(rawVelocity.x, core::_velocityDeadBand);
+        this->velocity.y = applyDeadband(rawVelocity.y, core::_velocityDeadBand);
+    }
 }
 
 // vector<Point> Entity::calcContour(Mat& frameInside){
@@ -27,8 +32,12 @@ Rect Entity::predictNextBoundingRect(){
     return prediction;
 }
 
-uint Entity::squareDistanceTo(Entity& e){
+uint Entity::squareDistanceTo(const Entity& e){
     return this->getBoundingRect().squareDistanceTo(e.getBoundingRect());
+}
+
+uint Entity::squareDistanceTo(const Rect& r){
+    return this->getBoundingRect().squareDistanceTo(r);
 }
 
 Rect Entity::predictPossibleLocations(){
@@ -37,35 +46,45 @@ Rect Entity::predictPossibleLocations(){
     const int& H = currentRect.height;
     const float& K = predictions::_offsetCoefficient; 
     const float& J = predictions::_sizeCoefficient;
-    const float M = 2;
+    // const float M = 3;
     Rect possibleLocations = Rect(currentRect.tl() - Point(K * W,  K * H), cv::Size2i(J * W, J * H));
-    if (this->velocity.x != 0 || this->velocity.y != 0){
-        uint16_t x = currentRect.tl().x + M * this->velocity.x;
-        uint16_t y = currentRect.tl().y + M * this->velocity.y;
-        Point A = Point(x, y);
-        x = currentRect.tl().x - M * this->velocity.x;
-        y = currentRect.tl().y - M * this->velocity.y;
-        Point B = Point(x, y);
-        cout << "tl: " << currentRect.tl() << " A: " << A << " B: " << B << " vel: " << this->velocity << "\n";
-        possibleLocations = Rect(A, B);
-    }
+    // if (this->velocity.x != 0 || this->velocity.y != 0){
+    //     Point A = Point(currentRect.tl().x + M * this->velocity.x, currentRect.tl().y + M * this->velocity.y);
+    //     Point B = Point(currentRect.tl().x - M * this->velocity.x, currentRect.tl().y - M * this->velocity.y);
+    //     possibleLocations = Rect(A, B);
+    // }
     return possibleLocations;
 }
 
-void matchEntity(vector<Entity>& currentEntities, vector<Entity>& currentRecognition){
+Entity generateEntity(Rect r, uint16_t type){
+	core::_startingId++;
+	return Entity(core::_startingId, type, r);
+}
 
-    for (uint16_t j = 0, size = MIN(currentEntities.size(), currentRecognition.size()); j < size; j++){
+void matchEntity(vector<Entity>& currentEntities, Recognition& currentRecognition, Mat& frame){
+    
+    for (Rect& r : currentRecognition.rects){
+        r.drawRect(frame, CV_RGB(0, 0, 0));
+    }
+
+    for (uint16_t j = 0, size = MIN(currentEntities.size(), currentRecognition.size); j < size; j++){
         Entity& currentEntity = currentEntities[j];
         uint distanceSquared = UINT32_MAX;
+        currentEntity.calcAndSetVelocity();
+        cout << "vel: " << currentEntity.getVelocity() << "\n";
         Rect possibleLocations = currentEntity.predictPossibleLocations();
+        possibleLocations.drawRect(frame, CV_RGB(255, 255, 255));
+    
         uint16_t matchingEntityIndex = NULL;
-        for (uint16_t i = 0, size = currentRecognition.size(); i < size; i++){
-            Entity& checkedEntity = currentRecognition[i];
 
-            if (currentEntity.getType() == checkedEntity.getType()) { 
-                uint currentDistanceSquared = currentEntity.squareDistanceTo(checkedEntity);
+        for (uint16_t i = 0, size = currentRecognition.size; i < size; i++){
+            const Rect& checkedRect = currentRecognition.rects[i];
+            
+            const uint16_t& checkedType = currentRecognition.types[i];
+            if (currentEntity.getType() == checkedType) { 
+                uint currentDistanceSquared = currentEntity.squareDistanceTo(checkedRect);
                 if (currentDistanceSquared < distanceSquared && 
-                    possibleLocations.contains(checkedEntity.getBoundingRect().tl())){
+                    possibleLocations.contains(checkedRect.tl())){
                     matchingEntityIndex = i;
                     distanceSquared = currentDistanceSquared;
                 }
@@ -73,10 +92,20 @@ void matchEntity(vector<Entity>& currentEntities, vector<Entity>& currentRecogni
         }
 
         if (matchingEntityIndex != NULL){
-            currentEntities[j].setBoundingRect(currentRecognition[matchingEntityIndex].getBoundingRect());
-            removeAtIndex(currentRecognition, matchingEntityIndex);
+            currentEntity.setBoundingRect(currentRecognition.rects[matchingEntityIndex]);
+            currentRecognition.remove(matchingEntityIndex);
+        }
+        else {
+            currentEntity.setBoundingRect(currentEntity.predictNextBoundingRect());
         }
     }
+
+    // if (currentRecognition.size > 0){
+    //     for (uint16_t i = 0; i < currentRecognition.size; i++){
+    //         currentEntities.emplace_back(generateEntity(currentRecognition.rects[i], currentRecognition.types[i]));
+    //     }
+    //     currentRecognition.clear();
+    // }
 }
 
 void Entity::draw(cv::Mat& frame, cv::Scalar color){
