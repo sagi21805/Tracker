aedd9a31e4fd8192ec4976cde044055912c98ed6
sagi <sagi21805@gmail.com>
2024-01-03 23:40:40 +0200

    added a (y\n) question for running

--- build.sh
+++ build.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 cmake -B ./build
-
 cd build 
-
 make -j4
-
 mv libTracker.so ../libTracker.so
+echo "[INFO]: Finished building"
+# Use double quotes around variables to prevent word splitting and globbing
+while true; do
+    read -p "Do you want to run Python? (y/n): " yn
+    case $yn in
+        [Yy]* ) 
+            cd ..
+            python3 main.py  # Replace 'main.py' with your actual Python script filename
+            break;;
+        [Nn]* ) 
+            echo "Exiting."
+            exit;;
+        * ) 
+            echo "Please answer yes or no.";;
+    esac
+done
 
-echo "[INFO]: Finished building running python!"
 
-cd ..
-
-sudo python3.11 main.py 
