3ee6c977600b4ae028919c89198d9c832beee94e
sagi <sagi21805@gmail.com>
2024-01-05 16:16:00 +0200

    Added the defualt (empty) case to be the Yes case

--- build.sh
+++ build.sh
@@ -6,16 +6,16 @@ mv libTracker.so ../libTracker.so
 echo "[INFO]: Finished building"
 # Use double quotes around variables to prevent word splitting and globbing
 while true; do
-    read -p "Do you want to run Python? (y/n): " yn
+    read -p "Do you want to run Python? (Y/n): " yn
     case $yn in
-        [Yy]* ) 
+        [Yy]*|"")  # Empty input or 'y'/'Y' is treated as 'yes'
             cd ..
             python3 main.py  # Replace 'main.py' with your actual Python script filename
             break;;
-        [Nn]* ) 
+        [Nn]* )
             echo "Exiting."
             exit;;
-        * ) 
+        * )
             echo "Please answer yes or no.";;
     esac
 done
