943f45890f7c0c38f62d3fdd29848eb9c2575028
sagi <sagi21805@gmail.com>
2024-01-09 12:41:08 +0200

    Defualt main file is starting with the Test video.

--- main.py
+++ main.py
@@ -1,29 +1,25 @@
 from python_utils.Tracker import Tracker
 from pynput import keyboard
 import threading
-#TEST CODE DONT TAKE SERIOUSLY
 
-
-tracker = Tracker("bumperWeights.pt", "/home/sagi21805/dcmp4.mp4")
+tracker = Tracker("bumperWeights.pt", "TestVidoe.mp4")
 is_running = True
 
 def main_code():
     while True:
         if is_running:
-            tracker.track()
+            tracker.track(True)
             
 
-# Function to be called when a key is pressed
 def on_press(key):
     global is_running
     try:
         if key.char == 'p':
-            is_running = not is_running  # Toggle the state
+            is_running = not is_running 
             print("Background code paused" if not is_running else "Background code resumed")
     except AttributeError:
         pass
 
-# Start the background thread
 listener = keyboard.Listener(on_press=on_press)
 
 listener_thread = threading.Thread(target=listener.start)
