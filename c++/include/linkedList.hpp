a52ce95925554674fb7e84c132be4b16edfb6f56
sagi <sagi21805@gmail.com>
2024-01-16 15:38:57 +0200

    Made Huge improvement to the linkedlist class, now works with any type of node

--- c++/include/linkedList.hpp
+++ c++/include/linkedList.hpp
@@ -1,130 +1,112 @@
 #ifndef _LinkedList_
 #define _LinkedList_
 
-#include "rect.hpp"
 #include <memory>
 
-typedef struct Node Node;
-typedef cv::Point2i Velocity2D;
-
-/**
- * @file linkedList.hpp
- * @brief The Node of the LinkedList.
- * @paragraph 
- *  The Node of the LinkedList which contains the 
- *  BoundingRect, velocity, and the ptr to the next Node. 
- *  
- * @author Sagi Or
-*/  
-struct Node{
-    
-    const Rect rect;
-    Velocity2D velocity;
-    std::shared_ptr<struct Node> next;
-
-///////////////////////////////////////// Constructors //////////////////////////////////////////////////
-
-    /**
-     * @file linkedList.hpp
-     * @brief Node constructor from raw data.
-     * @author Sagi Or
-    */
-    Node(Rect rect, Velocity2D velocity): rect(rect), velocity(velocity), next(nullptr) {}
-
-    /**
-     * @file linkedList.hpp
-     * @brief Node defualt constructor.
-     * @author Sagi Or
-    */
-    Node(): rect(Rect()), next(nullptr) {}
-
-};
+// Include the necessary header for the Node type
+#include "nodes.hpp"
 
 /**
  * @file linkedList.hpp
  * @brief The LinkedList class.
- * @paragraph 
+ * @paragraph
  *  The linkedList class contains an easy way to create a linked list 
- *  with a start and end ptr, append and prepend methods, and easy way to access data.
+ *  with a start and end ptr, append and prepend methods, and an easy way to access data.
  *  
  * @author Sagi Or
 */  
-class LinkedList{
+template<typename _Node> 
+class LinkedList {
 
-    public: 
-        std::shared_ptr<Node> start;
-        std::shared_ptr<Node> end;
-        size_t length = 0;
-
+public: 
+    std::shared_ptr<_Node> start;
+    std::shared_ptr<_Node> end;
+    size_t length = 0;
 
 ///////////////////////////////////////// Constructors //////////////////////////////////////////////////
 
-        /**
-         * @file linkedList.cpp
-         * @brief LinkedList constructor from raw data.
-         * @author Sagi Or
-        */
-        LinkedList(Rect rect, Velocity2D velocity);
+    LinkedList(_Node node) {
+        std::shared_ptr<_Node> newNode = std::make_shared<_Node>(node);
+        this->start = newNode;
+        this->end = newNode;
+        this->length++;
+    }
 
-        /**
-         * @file linkedList.cpp
-         * @brief LinkedList defualt constructor.
-         * @author Sagi Or
-        */
-        LinkedList();
+    LinkedList() {
+        std::shared_ptr<_Node> newNode = std::make_shared<_Node>();
+        this->start = newNode;
+        this->end = newNode;
+        this->length++;
+    }
 
-        /**
-         * @file linkedList.cpp
-         * @brief Gives access to items in the List.
-         * @param __i The index of the item to get from the list.
-         * @returns Reference to the node in the given index.
-         * @author Sagi Or
-        */
-        Node& getItem(size_t __i);
+    _Node& getItem(size_t __i) {
+        if (__i > this->length) {
+            throw std::runtime_error("Out of Bounds Index is \"" + std::to_string(__i) + "\" and the length is \"" + std::to_string(this->length) + "\"");
+        }
+        std::shared_ptr<_Node> ref = this->start;
 
-        /**
-         * @file linkedList.cpp
-         * @brief appends (adds to end) item to the end of the list.
-         * @param __item The item to append.
-         * @author Sagi Or
-        */
-        void append(Node __item);
+        for (size_t i = 0; i < __i; i++) {
+            ref = ref->next;
+        }
+        return *ref;
+    }
 
-        /**
-         * @file linkedList.cpp
-         * @brief Removes an item in a ceratin index.
-         * @param __i The index to remove the item from.
-         * @author Sagi Or
-        */
-        void remove(size_t __i);
+    void append(_Node node) {
+        std::shared_ptr<_Node> newNode = std::make_shared<_Node>(node); 
+        this->end->next = newNode;
+        this->end = newNode;
+        this->length++;
+    }
 
-        /**
-         * @file linkedList.cpp
-         * @brief prepends (adds to start) item to the end of the list.
-         * @param __item The item to prepend.
-         * @author Sagi Or
-        */
-        void prepend(Node __item);
+    void remove(size_t __i) {
+        if (__i >= this->length) {
+            throw "Out of Bounds";
+        }
+        std::shared_ptr<_Node> ref = this->start;
+        if (__i == 0) {
+            this->start = ref->next;
+            return;
+        }
+        for (size_t i = 0; ref != nullptr && i < __i - 1; i++) {
+            ref = ref->next;
+        }
+        std::shared_ptr<_Node> next = ref->next->next;
+        ref->next = next;
+        this->length--;
+    }
 
-        /**
-         * @file linkedList.cpp
-         * @brief Insert certain item in a certain index.
-         * @param __i The index of the item to get from the list.
-         * @param __item The item to insert.
-         * @returns Reference to the node in the given index.
-         * @author Sagi Or
-        */
-        void insert(size_t  __i, Node __item);
+    void prepend(_Node node) {
+        std::shared_ptr<_Node> newNode = std::make_shared<_Node>(node); 
+        newNode->next = this->start;    
+        this->start = newNode;
+        this->length++;
+    }
 
-        /**
-         * @file linkedList.cpp
-         * @brief Gives access to items in the List.
-         * @param __i The index of the item to get from the list.
-         * @returns Reference to the node in the given index.
-         * @author Sagi Or
-        */
-        Node& operator[](size_t __i);
+    void insert(size_t __i, _Node node) {
+        if (__i >= this->length) {
+            throw "Out of Bounds";
+        }
+        if (__i == 0) {
+            this->prepend(node);
+            return;
+        }
+        if (__i == this->length - 1) {
+            this->append(node);
+            return;
+        }
+        std::shared_ptr<_Node> ref = this->start;
+        std::shared_ptr<_Node> newNode = std::make_shared<_Node>(node); 
+        for (size_t i = 0; ref != nullptr && i < __i - 1; i++) {
+            ref = ref->next;
+        }
+        newNode->next = ref->next;
+        ref->next = newNode;
+        this->length++;
+    }
 
+    _Node& operator[](size_t __i) {
+        return this->getItem(__i);
+    }
 };
 
 #endif
