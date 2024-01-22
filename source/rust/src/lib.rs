f5b354a4b266fca1578ab246ee8b65022918202b
sagi <sagi21805@gmail.com>
2024-01-22 21:32:56 +0200

    Tested rust integration with this code successfully!

--- source/rust/src/lib.rs
+++ source/rust/src/lib.rs
@@ -1,16 +1,21 @@
 use std::os::raw::c_int;
 
+#[allow(
+    non_camel_case_types,
+    non_upper_case_globals,
+    non_snake_case
+)]
 
 #[no_mangle]
-pub extern "C" fn print_array(ptr: *const c_int, len: usize){
+pub extern "C" fn printArray(ptr: *const c_int, len: usize){
     // Safety: We assume the pointer is valid for the specified length
-    let slice = unsafe {
+    let slice: &[i32] = unsafe {
         std::slice::from_raw_parts(ptr as *const i32, len)
     };
-    let rust_vector: Vec<i32> = slice.to_vec();
-    
-    for item in rust_vector{
-        print!("{} ", item);
+    let rustVector: Vec<i32> = slice.to_vec();
+    for item in rustVector{
+        print!("{} ", item + 2);
     }
     println!("");
 }
+
