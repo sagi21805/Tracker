eeb8c98a09216ce91f5b4526ff2a81e25ca731c4
sagi <sagi21805@gmail.com>
2024-01-29 19:32:47 +0200

    Added a split by threshold function

--- source/c++/include/boundingBox.hpp
+++ source/c++/include/boundingBox.hpp
@@ -2,24 +2,23 @@
 #define _BoundingBox_
 
 #include "rect.hpp"
-
 struct BoundingBox {
     
     Rect rect;
     uint16_t type;
-    float32 confidance;
+    float32 confidence;
  
-    BoundingBox(Rect rect, uint16_t type, float32 confidance): rect(rect), type(type), confidance(confidance) {}
-    BoundingBox(const BoundingBox& box) : rect(box.rect), type(box.type), confidance(box.confidance) {}
-    BoundingBox() : rect(Rect()), type(UINT16_MAX), confidance(0) {}
+    BoundingBox(Rect rect, uint16_t type, float32 confidence): rect(rect), type(type), confidence(confidence) {}
+    BoundingBox(const BoundingBox& box) : rect(box.rect), type(box.type), confidence(box.confidence) {}
+    BoundingBox() : rect(Rect()), type(UINT16_MAX), confidence(0) {}
 
     bool isEmpty();
 
-    bool isIntersecting(BoundingBox& b, float32 percentageAllowed);
 };
 
-extern "C" bool _isIntersecting(BoundingBox& b1, BoundingBox& b2, float32 percentageAllowed);
+vector<BoundingBox> splitByThreshold(vector<BoundingBox>& boundingBoxes, double threshold);
 
-vector<BoundingBox> generateBoundingBoxes(int32_t* points, uint16_t* types, float32* confidances, uint16_t size);
+vector<BoundingBox> generateBoundingBoxes(int32_t* points, uint16_t* types, float32* confidences, uint16_t size);
+
 #endif
 
--- source/c++/src/boundingBox.cpp
+++ source/c++/src/boundingBox.cpp
@@ -1,13 +1,13 @@
 #include "boundingBox.hpp"
 
-vector<BoundingBox> generateBoundingBoxes(int32_t* points, uint16_t* types, float32* confidances, uint16_t size) {
+vector<BoundingBox> generateBoundingBoxes(int32_t* points, uint16_t* types, float32* confidences, uint16_t size) {
     vector<BoundingBox> boundingBoxes;
     boundingBoxes.reserve(size);
 	for (uint16_t i = 0; i < size; i++){
         int32_t* currentPoint = points + i*core::_elementsInPoint;
         uint16_t currentType = types[i];
-        float32 currentConfidance = confidances[i];
-        boundingBoxes.emplace_back(Rect(currentPoint), currentType, currentConfidance);
+        float32 currentconfidence = confidences[i];
+        boundingBoxes.emplace_back(Rect(currentPoint), currentType, currentconfidence);
 	}
 
     return boundingBoxes;
@@ -17,6 +17,10 @@ bool BoundingBox::isEmpty(){
     return this->rect.empty();
 }
 
-bool BoundingBox::isIntersecting(BoundingBox& b, float32 percentageAllowed) {
-    return _isIntersecting(*this, b, percentageAllowed);
+vector<BoundingBox> splitByThreshold(vector<BoundingBox>& boundingBoxes, double threshold) {    
+    uint16_t count = 0;
+    uint16_t i = boundingBoxes.size() - 1;
+    while (boundingBoxes[i].confidence < threshold){ count++; i--;}
+    boundingBoxes = vector<BoundingBox>(boundingBoxes.begin(), boundingBoxes.end() - count);
+    return vector<BoundingBox>(boundingBoxes.end() - count, boundingBoxes.end());
 }
