66a6083aba715158692c84d6e5c4d6d280ac4275
sagi <sagi21805@gmail.com>
2024-02-07 16:11:35 +0200

    overided the contains function on rect

--- source/c++/include/rect.hpp
+++ source/c++/include/rect.hpp
@@ -72,7 +72,7 @@ class Rect : public cv::Rect2i{
     float32 iouPercentage(const Rect& r) const;
 
     template<typename T> 
-    bool Rect::contains(const cv::Point_<T>& pt) const {
+    bool contains(const cv::Point_<T>& pt) const {
         return x <= pt.x && pt.x <= x + width && y <= pt.y && pt.y <= y + height;
     }
 
