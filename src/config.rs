ae9ae0fc56961d32f96dc3b448ad12708c3d487d
sagi <sagi21805@gmail.com>
2024-10-13 01:04:11 +0300

    Added a deafault configuration in case file don't exist

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,9 @@
 use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::Read;
+use std::path::Path;
+
+use crate::config;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Config {
@@ -16,10 +19,31 @@ pub struct Config {
 impl Config {
     // Load config from a JSON file
     pub fn from_json_file(file_path: &str) -> Result<Self, Box<dyn std::error::Error>> {
-        let mut file = File::open(file_path).expect("Can't find configuration file");
-        let mut data = String::new();
-        file.read_to_string(&mut data)?;
-        let config: Config = serde_json::from_str(&data)?;
-        Ok(config)
+        if Path::new(file_path).exists() {
+            let mut file = File::open(file_path).expect("Can't find configuration file");
+            let mut data = String::new();
+            file.read_to_string(&mut data)?;
+            let config: Config = serde_json::from_str(&data)?;
+            Ok(config)
+        } else {
+            println!("Configuration file not found, loading default configuration");
+            println!("Configuration is {:?}", Config::default());
+            std::thread::sleep(std::time::Duration::from_secs(2));
+            Ok(Config::default())
+        }
+    }
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            elements_in_point: 4,
+            min_score: 0.5,
+            move_to_last_seen: 25,
+            min_confidence: 0.0,
+            size_coefficient: 0.75,
+            vel_coefficient: 2.0,
+            ratio: 0.1,
+        }
     }
 }
